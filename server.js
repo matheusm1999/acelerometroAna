06571f21c2a3e2063ada3e3c1c1474628936f417
matheusm1999 <matheusm1999@live.com>
2021-06-09 03:42:51 -0300

    teste

--- server.js
+++ server.js
@@ -8,16 +8,14 @@ const INDEX = '/index.html';
 
 const server = express()
   .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-	server.get('/', function(req,res){
+  .listen(PORT, () => console.log(`Listening on ${PORT}`)
+	.get('/', function(req,res){
 		res.sendFile('transmissao.html',{root: __dirname})
 	});
-
-	server.get('/index', function(req,res){
+	.get('/index', function(req,res){
 		res.sendFile('index.html',{root: __dirname})
 	});
-
+);
 const wss = new Server({ server });
 
 let sockets = [];
