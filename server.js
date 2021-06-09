1a0bcf387f6ca8dae5b5c5bce10a14bfe7931d43
matheusm1999 <matheusm1999@live.com>
2021-06-09 03:55:45 -0300

    teste

--- server.js
+++ server.js
@@ -6,10 +6,9 @@ const { Server } = require('ws');
 const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
-const server = express();
-
-server.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-server.listen(PORT, () => console.log('Servidor Funcionando'));
+const server = express()
+	.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+	.listen(PORT, () => console.log('Servidor Funcionando'));
 
 /*
 server.get('/', function(req,res){
